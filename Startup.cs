1a3b8477ac33f5487f0257fd4a4c2318f39841cb
闵文辉 <mwh0808@qq.com>
2019-07-24 10:51:16 +0800

    添加测试控制台,优化代码,格式化代码

---
+++ Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPinyin
+{
+    class Startup
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            string str;
+
+            Console.WriteLine("--------------------  PinYin Test  --------------------");
+            Console.WriteLine("说明：请输入要翻译的中文，示例如下。");
+            Console.Write("->> ");
+            Console.WriteLine(str = "中文翻译测试一下，效果如何？");
+                Console.WriteLine($"    {Pinyin.GetInitials(str)} | {Pinyin.GetPinyin(str)}");
+            Console.WriteLine();
+
+        readline:
+            Console.Write("->> ");
+            str = Console.ReadLine();
+
+            if (str.ToLower() != "exit")
+            {
+                Console.WriteLine($"    {Pinyin.GetInitials(str)} | {Pinyin.GetPinyin(str)}");
+                Console.WriteLine();
+                goto readline;
+            }
+        }
+
+    }
+}
